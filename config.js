aa83484258ad70bb7002bad658c63cf1ef7e1144
Alam <13517096@std.stei.itb.ac.id>
2019-11-23 21:07:25 +0700

    timezone bugfix, now timezone is set to UTC for uniformity

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 // Setting DB yang nanti ditambahin ke gitignore supaya gak ganti-ganti terus setiap push
 // Di local jalanin "git rm --cached config.js" supaya ke ignore
 const db_config = {
-  host: "localhost",
-  user: "abda",
-  password: "abda",
-  database: "transaksi_tiket"
+  host: "transactionservicedb.cmqcolum2oyw.us-east-1.rds.amazonaws.com",
+  user: "admin",
+  password: "adminadmin",
+  database: "transaksi_tiket",
+  timezone: "Z"
 };
 
 module.exports = db_config;
